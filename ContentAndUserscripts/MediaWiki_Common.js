5d73208a93d223eab72e42b3ef11783521956029
jan <d_jan@ymail>
2013-05-11 21:51:50 +0200

    html structure and value retrieval

--- ContentAndUserscripts/MediaWiki_Common.js
+++ ContentAndUserscripts/MediaWiki_Common.js
@@ -33,7 +33,24 @@ var customizeToolbar = function() {
 $.wikiEditor.modules.dialogs.modules['mytool'] = {
 				titleMsg: 'wikieditor-toolbar-tool-mytool-title',
 				id: 'wikieditor-toolbar-mytool-dialog',
-				html: '<div></div>',
+				html: '\
+				<div id="wikieditor-toolbar-mytool-step1Container"> <!-- what do you want to do wrapper-->\
+				<!-- wizard buttons go here-->\
+				</div>\
+				<div id="wikieditor-toolbar-mytool-step2Container">\
+				<div>\
+				<!-- insert image goes here-->\
+			</div>\
+			<div>\
+				<!-- generatete Link goes here-->\
+			</div>\
+			<fieldset>\
+				<input type="text" id="wikieditor-toolbar-mytool-inputFilename" name="filename">\
+				<label id="wikieditor-toolbar-mytool-lableFilename"for="filename">Filename</label>\
+				<input type="text" id="wikieditor-toolbar-mytool-inputCaption" name="caption">\
+				<label id="wikieditor-toolbar-mytool-lableCaption"for="caption">Caption</label>\
+			</fieldset>\
+			</div>',
 				init: function () {
 					console.log("init Started");
 				},
@@ -45,7 +62,7 @@ $.wikiEditor.modules.dialogs.modules['mytool'] = {
 						{
 							'text':'insert', 
 							'click':function () {
-							var fileUse = "yey! teststring";
+							var fileUse = $('this').find('#wikieditor-toolbar-mytool-inputFilename').val();
 							$( this ).dialog( 'close' );
 							$.wikiEditor.modules.toolbar.fn.doAction(
 									$( this ).data( 'context' ),
